9ac6e90b5bcd01c654e7f68cd0c4cd2c9d21b221
Ahmed Bodiwala <ahmed_bodi@msn.com>
2017-07-12 00:10:10 +0100

    Add Masternode Payment reduction blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1786,21 +1786,18 @@ CAmount GetBlockSubsidy(int nPrevBits, int nPrevHeight, const Consensus::Params&
 
 CAmount GetMasternodePayment(int nHeight, CAmount blockValue)
 {
-    CAmount ret = blockValue/5; // start at 20%
+    CAmount ret = 10000 * COIN; // start at 10,000 TRC
 
-    int nMNPIBlock = Params().GetConsensus().nMasternodePaymentsIncreaseBlock;
-    int nMNPIPeriod = Params().GetConsensus().nMasternodePaymentsIncreasePeriod;
-
-                                                                      // mainnet:
-    if(nHeight > nMNPIBlock)                  ret += blockValue / 20; // 158000 - 25.0% - 2014-10-24
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 1)) ret += blockValue / 20; // 175280 - 30.0% - 2014-11-25
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 2)) ret += blockValue / 20; // 192560 - 35.0% - 2014-12-26
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 3)) ret += blockValue / 40; // 209840 - 37.5% - 2015-01-26
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 4)) ret += blockValue / 40; // 227120 - 40.0% - 2015-02-27
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 5)) ret += blockValue / 40; // 244400 - 42.5% - 2015-03-30
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 6)) ret += blockValue / 40; // 261680 - 45.0% - 2015-05-01
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 7)) ret += blockValue / 40; // 278960 - 47.5% - 2015-06-01
-    if(nHeight > nMNPIBlock+(nMNPIPeriod* 9)) ret += blockValue / 40; // 313520 - 50.0% - 2015-08-03
+    if(nHeight < Params().GetConsensus().nMasternodeReduction[0]) ret = 10000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[1]) ret = 9000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[2]) ret = 8000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[3]) ret = 7000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[4]) ret = 6000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[5]) ret = 5000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[6]) ret = 4000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[7]) ret = 3000 * COIN;
+    if(nHeight > Params().GetConsensus().nMasternodeReduction[8]) ret = 2000 * COIN;
+    if(nHeight < Params().GetConsensus().nMasternodeReduction[9]) ret = 1000 * COIN;
 
     return ret;
 }
